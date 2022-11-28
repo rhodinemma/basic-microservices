afb22f67bdd06428365d5805638b57d90a207456
rhodinemma <rhodinemma10@gmail.com>
2022-11-28 16:28:03 +0300

    ft(render-comment-status)

--- comments/index.js
+++ comments/index.js
@@ -44,7 +44,7 @@ app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   if (type === "CommentModerated") {
-    const { postId, id, status } = data;
+    const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
 
     const comment = comments.find((comment) => {
@@ -53,7 +53,7 @@ app.post("/events", async (req, res) => {
 
     comment.status = status;
 
-    await axios.post("http://localhost:4005", {
+    await axios.post("http://localhost:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
