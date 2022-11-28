942a6129ec17e53628c04a6a1ccd8c644bbd7224
rhodinemma <rhodinemma10@gmail.com>
2022-11-28 13:24:33 +0300

    ft(comment-moderation)

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  // send out events to various services
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
-  //axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4002/events", event);
 
   res.send({ status: "OK" });
 });
--- query/index.js
+++ query/index.js
@@ -6,9 +6,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
+// Example
+/* 
+    posts === {
+        'some post id' : {
+            id: 'some post id',
+            title: 'post title',
+            comments: [
+                { id: 'some comment id', content: 'some comment!' }
+            ]
+        }
+    }
+*/
 
-app.post("/events", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on port 4002");
